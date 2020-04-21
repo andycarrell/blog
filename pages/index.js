698c924e8efb6b6aed7aa890bbf34d4a9a840fd7
Andy Carrell <andy@jasper.io>
2020-04-21 18:07:01 +1200

    Uses shared components instead of definitions in actual file

--- pages/index.js
+++ pages/index.js
@@ -1,113 +1,16 @@
-import {
-  Box,
-  IconButton,
-  Text,
-  Stack,
-  Link,
-  useClipboard,
-} from "@chakra-ui/core";
+import { Box, Text, Stack, Code as ChakraCode } from "@chakra-ui/core";
 
 import Page from "../components/Page";
 import useTitle from "../hooks/useTitle";
-
-const ExternalLink = ({ children, href, ...rest }) => (
-  <Link {...rest} href={href} color="cyan.700" isExternal>
-    {children}
-  </Link>
-);
-
-const Heading1 = ({ children, ...rest }) => (
-  <Text
-    {...rest}
-    textAlign="center"
-    mx="auto"
-    fontWeight="medium"
-    fontFamily="heading"
-    fontSize={["4xl", "4xl", "5xl", "5xl"]}
-    color="gray.700"
-    as="h1"
-  >
-    {children}
-  </Text>
-);
-
-const Heading2 = ({ children, ...rest }) => (
-  <Text
-    {...rest}
-    fontWeight="medium"
-    fontFamily="heading"
-    fontSize={["2xl", "2xl", "3xl", "3xl"]}
-    color="gray.600"
-    as="h2"
-  >
-    {children}
-  </Text>
-);
-
-const Paragraph = ({ children, ...rest }) => (
-  <Text {...rest} as="p" lineHeight="tall">
-    {children}
-  </Text>
-);
-
-const UnorderedList = ({ children, ...rest }) => (
-  <Stack {...rest} spacing={2} as="ul" lineHeight="tall">
-    {children}
-  </Stack>
-);
-
-UnorderedList.Item = ({ children, ...rest }) => (
-  <Text {...rest} marginLeft="32px" as="li">
-    {children}
-  </Text>
-);
-
-const Code = ({ children, ...rest }) => {
-  const { onCopy, hasCopied } = useClipboard(children);
-
-  return (
-    <Box {...rest} backgroundColor="gray.50" position="relative" padding={4}>
-      <Text
-        whiteSpace="pre"
-        fontSize="sm"
-        lineHeight="tall"
-        borderRadius="md"
-        overflowX="scroll"
-      >
-        <code>{children}</code>
-      </Text>
-      <Box as="span" position="absolute" bottom="12px" right="12px">
-        <IconButton
-          size="sm"
-          onClick={onCopy}
-          aria-label={hasCopied ? "Copied" : "Copy"}
-          title={hasCopied ? "Copied" : "Copy"}
-          icon={hasCopied ? "check" : "copy"}
-        />
-      </Box>
-    </Box>
-  );
-};
-
-Code.Inline = ({ children }) => (
-  <Text
-    as="span"
-    borderRadius="sm"
-    backgroundColor="gray.50"
-    fontSize="sm"
-    lineHeight="tall"
-    wordBreak="break-all"
-    padding={1}
-  >
-    <code>{children}</code>
-  </Text>
-);
-
-const Quote = ({ children, ...rest }) => (
-  <Text {...rest} fontSize="lg" as="em" color="gray.600">
-    {children}
-  </Text>
-);
+import {
+  Code,
+  Quote,
+  Heading1,
+  Heading2,
+  Paragraph,
+  ExternalLink,
+  UnorderedList,
+} from "../components/typography";
 
 export default function Index() {
   useTitle("andycarrell > Blog");
@@ -190,7 +93,7 @@ export default function Index() {
           the "
           <ExternalLink href="https://github.com/prisma-labs/graphql-playground/">
             playground
-          </ExternalLink>{" "}
+          </ExternalLink>
           ". We can access the entire schema at:
         </Paragraph>
         <Box textAlign="center" marginX="auto" padding={2}>
