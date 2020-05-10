4e7e6d2a6f73556a99e8f0027a92682d7622cf4b
Andy Carrell <andy@jasper.io>
2020-05-10 17:36:56 +1200

    Makes blog post rendering extensible - based on a slug. Removes hard coded page.

---
+++ pages/posts/[slug].js
@@ -0,0 +1,38 @@
+import Error from "next/error";
+import { useRouter } from "next/router";
+
+import Page from "../../components/Page";
+import useTitle from "../../hooks/useTitle";
+import Post1 from "../../posts/Post1";
+
+const postFor = {
+  "mocking-graphql-in-cypress": [Post1, "Mocking GraphQL in Cypress"],
+};
+
+export function getStaticPaths() {
+  return {
+    paths: Object.keys(postFor).map((s) => `/posts/${s}`),
+    fallback: false,
+  };
+}
+
+export function getStaticProps() {
+  return { props: {} };
+}
+
+export default function Index() {
+  const { query } = useRouter();
+  const [Post, title] = postFor[query.slug];
+
+  useTitle(`andycarrell > ${title || "Blog"}`);
+
+  if (Post) {
+    return (
+      <Page>
+        <Post />
+      </Page>
+    );
+  }
+
+  return <Error statusCode={404} />;
+}
